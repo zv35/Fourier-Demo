d18935a3f40af8bee3cb22f65600dbde908493c5
zakv <zvladich6564@gmail.com>
2022-03-26 18:27:13 -0700

    Updated the about page

--- fourier.js
+++ fourier.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
+canvas.width = window.innerWidth - 10; // Gap to remove side scroll bar
 
 // Settings
 var fps = 24;
